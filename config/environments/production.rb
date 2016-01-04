9ff85197c85d451c296c01d44f425b13a4c8d44b
Suli Huang <huangsl1994@gmail.com>
2016-01-04 22:45:21 +0000

    integrated AmazonS3 with Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   
   config.action_mailer.default_url_options = {host:'ror-project233.herokuapp.com'}
   
-  config.paperclip_default = {
+  config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
       :bucket => ENV['AWS_BUCKET'],
