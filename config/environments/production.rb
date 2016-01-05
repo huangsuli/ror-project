26bcb695f42c9fa74861e523c96ce3ce737e10b8
Suli Huang <huangsl1994@gmail.com>
2016-01-05 00:37:29 +0000

    fixing problem of amazonS3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,9 +82,9 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => 'ror-project233',
-      :access_key_id => 'AKIAJNAJTU3WAKKL2DGA',
-      :secret_access_key => '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'
+      :bucket => ENV['S3_BUCKET_NAME']
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
 end
